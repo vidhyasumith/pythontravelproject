5ca34d37278e18d98a880c7a3a74574742a811a8
vidhyasumith <vidhyasumith2022@gmail.com>
2022-01-25 14:20:27 +0530

    fourth commit

---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import place
+from .models import team
+
+# Create your views here.
+
+def demo(request):
+    obj=place.objects.all()
+    obj1=team.objects.all()
+    return render(request, "index.html",{'result': obj ,'result1': obj1})
